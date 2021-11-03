f348f3f33d30ece7f68071b14db083a2b5d5aba0
Andrew <blurglecruncheon@gmail.com>
2021-11-03 12:53:33 -0500

    quick finder modified to try several files

--- utils/2l.py
+++ utils/2l.py
@@ -1,17 +1,20 @@
-count = 0
-this_count = 0
+def print_out(my_file):
+    count = 0
+    this_count = 0
+    x_last = -1
+    with open(my_file) as file:
+        for (line_count, line) in enumerate (file, 1):
+            ll = line.lower().strip()
+            #if len(ll) % 2: continue
+            x = (len(ll) + 1) // 2 - 1
+            if ll[:x] == ll[-x:]:
+                if x != x_last:
+                    x_last = x
+                    this_count = 0
+                count += 1
+                this_count += 1
+                print(this_count, count, ll, ll[x:-x])
 
-x_last = -1
-
-with open("c:/writing/dict/brit-1word.txt") as file:
-	for (line_count, line) in enumerate (file, 1):
-		ll = line.lower().strip()
-		#if len(ll) % 2: continue
-		x = (len(ll) + 1) // 2 - 1
-		if ll[:x] == ll[-x:]:
-			if x != x_last:
-				x_last = x
-				this_count = 0
-			count += 1
-			this_count += 1
-			print(this_count, count, ll, ll[x:-x])
+#print_out("c:/writing/dict/brit-1word.txt")
+print_out("c:/writing/dict/firsts.txt")
+print_out("c:/writing/dict/lasts.txt")
