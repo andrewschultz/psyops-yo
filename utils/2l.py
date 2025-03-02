adf83196fc5436a257454c10447bb0c7b7325098
Andrew <blurglecruncheon@gmail.com>
2025-03-02 15:50:33 -0600

    2l.py output is more readable now

--- utils/2l.py
+++ utils/2l.py
@@ -1,7 +1,8 @@
-def print_out(my_file):
+def print_out(my_file, long_first = False):
     count = 0
     this_count = 0
     x_last = -1
+    print("Parsing", my_file)
     with open(my_file) as file:
         for (line_count, line) in enumerate (file, 1):
             ll = line.lower().strip()
@@ -13,8 +14,9 @@ def print_out(my_file):
                     this_count = 0
                 count += 1
                 this_count += 1
-                print(this_count, count, ll, ll[x:-x])
+                my_string = "{} {}".format(ll[x:-x].title(), ll.title()) if long_first else "{} {}".format(ll.title(), ll[x:-x].title())
+                print("\"{}\" {} {}".format(my_string, this_count, count))
 
 #print_out("c:/writing/dict/brit-1word.txt")
-print_out("c:/writing/dict/firsts.txt")
-print_out("c:/writing/dict/lasts.txt")
+print_out("c:/writing/dict/firsts.txt", long_first = True)
+print_out("c:/writing/dict/lasts.txt", long_first = False)
